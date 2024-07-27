b8b9e4203d5f1528e0d647e7cbbbbad4f072b145
anfastech <kpanfas6@gmail.com>
2024-07-27 23:49:04 +0530

    fix darkmode bug

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,10 @@
+// theme.config.tsx
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: (
     <>
-      {/* <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M50 10L90 90L10 90L50 10Z" fill="currentColor" />
-      </svg> */}
       <span style={{ marginLeft: ".4em", fontWeight: 900, fontSize: "1.5rem" }}>
         Cyrea Code
       </span>
@@ -17,12 +15,12 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/anfastech/cyera-docs/tree/main",
   footer: {
-    text: "Cyera Gibit Corp",
+    text: "Cyeranex Tech",
   },
+  darkMode: true,
   sidebar: {
     defaultMenuCollapseLevel: 1,
   },
-  darkMode: true,
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
